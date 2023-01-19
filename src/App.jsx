e380523f97c344e9cf782bfda1d5bafe05f0c14c
Sushil1204 <sushilspundkar@gmail.com>
2023-01-20 00:53:05 +0530

    wordleContext created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import React from "react";
-import Grid from "./components/Grid/Grid";
-import KeyBoard from "./components/Keyboard/KeyBoard";
+import React, { useState } from "react";
+import Board from "./components/Board";
+import { createContext } from "react";
+import words from "./words";
+import _ from 'lodash';
+
+export const WordleContext = createContext()
 
 function App() {
+  const [word, setWord] = useState(_.sample(words).toUpperCase());
+  const [guessWord, setGuessWord] = useState('');
+  const [completedRows, setCompletedRows] = useState([]);
+  const [currentRow, setCurrentRow] = useState(0);
+
+  function guessTheWord(char) {
+    if (guessWord.length === 5) return
+    setGuessWord(guessWord.concat(char))
+  }
+
+  function pressEnter() {
+    if (guessWord.length < 5) return
+    if (!words.includes(guessWord.toLowerCase())) return alert("Word not found !");
+    if (guessWord == word) alert("You Won 😃 ");
+    if(currentRow > 5) return alert('You have unfortunately exhausted all your trials. Press refresh to try again. ')
+
+    setCurrentRow(currentRow + 1);
+    setCompletedRows([...completedRows, currentRow]);
+    setGuessWord("");
+  }
+  function backspace() {
+    setGuessWord(guessWord.slice(0, guessWord.length - 1));
+  }
   return (
-    <div className="">
-      <Grid />
-      <KeyBoard/>
-    </div>
+    <WordleContext.Provider value={{ guessTheWord ,
+      pressEnter ,
+       completedRows ,
+        currentRow, 
+        word ,
+        guessWord ,
+         backspace }}>
+<Board/>
+    </WordleContext.Provider>
   );
 }
 
